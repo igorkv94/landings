f8ac784d015746bceee8c472d4b43297d89de4ed
DESKTOP-ABS8CCH\User <igorkv94@gmail.com>
2017-08-01 09:11:02 +0300

    Add a new site and optimize for pagespeed

---
+++ landingpage/app/js/custom.js
@@ -0,0 +1,113 @@
+var tag = document.createElement('script');
+tag.src = 'https://www.youtube.com/player_api';
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var tv,
+    playerDefaults = {autoplay: 0, autohide: 1, modestbranding: 0, rel: 0, showinfo: 0, controls: 0, disablekb: 1, enablejsapi: 0, iv_load_policy: 3};
+var vid = [
+        {'videoId': 'e8DFN3m8XGQ', 'suggestedQuality': 'hd720'},
+    ],
+    randomVid = 0,
+    currVid = randomVid;
+
+function onYouTubePlayerAPIReady(){
+    tv = new YT.Player('tv', {events: {'onReady': onPlayerReady, 'onStateChange': onPlayerStateChange}, playerVars: playerDefaults});
+}
+
+function onPlayerReady(){
+    tv.loadVideoById(vid[currVid]);
+    tv.seekTo(2);
+    tv.mute();
+    $('#tv').addClass('screen__active');
+    $('.play').toggleClass('pause');
+    $('.speaker').toggleClass('speaker__mute');
+}
+
+function onPlayerStateChange(e) {
+}
+
+function vidRescale(){
+
+    var w = $(window).width()+200,
+        h = $(window).height()+200;
+
+    if (w/h > 16/9){
+        tv.setSize(w, w/16*9);
+        $('.tv .screen').css({'left': '0px'});
+    } else {
+        tv.setSize(h/9*16, h);
+        $('.tv .screen').css({'left': '0px'});
+    }
+}
+
+$(window).on('load resize', function(){
+    vidRescale();
+});
+
+$('.play').on('click', function () {
+    $(this).toggleClass('pause');
+    if($('.play').hasClass('pause')){
+        tv.playVideo();
+    } else {
+        tv.pauseVideo();
+    }
+});
+
+$('.speaker').click(function(e) {
+    $(this).toggleClass('mute');
+    if($('.speaker').hasClass('mute')){
+        tv.mute();
+    } else {
+        tv.unMute();
+    }
+});
+
+$(document).ready(function(){
+    new WOW().init();
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots:false,
+        navText : ["<",">"],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+
+    $('.navbar-nav>li>a').click( function(){
+        var scroll_el = $(this).attr('href');
+        if ($(scroll_el).length != 0) {
+            $('html, body').animate({ scrollTop: ($(scroll_el).offset().top-60) }, 500);
+            $('.navbar-nav>.active').toggleClass('active');
+        }
+        return false;
+    });
+    var time = 0;
+    var comment = [
+        "Aliquam erat volutpat. In rhoncus viverra arcu, sed tristique dui ultrices nec. Nulla accumsan tincidunt magna, sed commodo purus vulputate a.",
+        "Donec non tellus tellus. Integer dignissim nibh vel tortor pretium tempor. In pulvinar placerat erat nec ullamcorper. Etiam efficitur tempus massa.",
+        "Interdum et malesuada fames ac ante ipsum primis in faucibus. Sed magna velit, sollicitudin rutrum volutpat id, cursus eu tortor."
+    ];
+    var author = ["Eddard Stark", "Daenerys Targaryen", "Tyrion Lannister"];
+    var author_id = ["@#eddardstark", "@#daenerystargaryen", "@#tyrionlannister"];
+
+    function swapComment() {
+        $('#comments_comment').text("@"+comment[time]);
+        $('#comments__author__img').attr("src", "img/users/user-"+time+".jpg");
+        $('#comments__author__img').attr("alt", "user-"+(time+1));
+        $('#comments__author__text').html(author[time]+" <span>"+author_id[time]+"</span>");
+        time++;
+        if(time>2)  time=0;
+    }
+    setInterval(swapComment, 3000);
+
+});
---
+++ landingpage/gulpfile.js
@@ -0,0 +1,153 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
+var uncss = require('gulp-uncss');
+var concat = require('gulp-concat');
+var connect = require('gulp-connect');
+var buffer = require('vinyl-buffer');
+var imagemin = require('gulp-imagemin');
+var merge = require('merge-stream');
+var spritesmith = require('gulp.spritesmith');
+var fileinclude = require('gulp-file-include');
+var minify = require('gulp-minify');
+var htmlmin = require('gulp-htmlmin');
+var gutil = require('gulp-util');
+var critical = require('critical').stream;
+var src = 'app';
+var dst = 'dist';
+
+
+gulp.task('init-styles', function() {
+    gulp.src(src+'/scss/bootstrap.scss')
+        .pipe(sass().on('error', sass.logError))
+		.pipe(uncss({
+		  html: [
+			'http://localhost:8888/'
+		  ],
+			timeout: 1000
+		}))
+		.pipe(autoprefixer({
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
+		.pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(dst+'/css/'))
+});
+
+gulp.task('styles',['init-styles'], function() {
+    gulp.src(src+'/scss/main.scss')
+        .pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer({
+            browsers: ['last 3 versions'],
+            cascade: false
+        }))
+		.pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(dst+'/css/'))
+});
+
+gulp.task('concat-styles',['styles'], function() {
+    gulp.src(dst+'/css/*.css')
+        .pipe(concat('style.min.css'))
+        .pipe(gulp.dest(dst+'/css/'))
+		.pipe(connect.reload());
+});
+
+
+gulp.task('critical', function () {
+    return gulp.src('dist/index.html')
+        .pipe(critical({
+			inline: true,
+			base: 'dist/',
+			css: ['dist/css/style.min.css'],
+			dimensions: [{
+			  width: 320,
+			  height: 480
+			},{
+			  width: 768,
+			  height: 1024
+			},{
+			  width: 1280,
+			  height: 960
+			}],
+			dest: 'dist/critical.html',
+			minify: true,
+			extract: false,
+			ignore: ['font-face']
+			})) 
+		.on('error', function(err) { gutil.log(gutil.colors.red(err.message)); });
+});
+
+gulp.task('images', function() {
+    gulp.src(src+'/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest(dst+'/img/'))
+});
+
+gulp.task('html', function() {
+    gulp.src(src+'/*.html')
+        .pipe(fileinclude({
+                prefix: '@@',
+                basepath: '@file'
+            }))
+		.pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest(dst+"/"))
+		.pipe(connect.reload());
+});
+
+gulp.task('js', function() {
+    gulp.src(src+'/js/**/*')
+        .pipe(minify({
+			ext:{
+				src:'-debug.js',
+				min:'-min.js'
+			},
+        exclude: ['tasks'],
+        ignoreFiles: ['.combo.js', '-min.js']
+		}))
+		.pipe(gulp.dest(dst+'/js'));
+});
+
+gulp.task('fonts', function() {
+    gulp.src([src+'/fonts/*'])
+        .pipe(gulp.dest(dst+'/fonts/'))
+});
+
+gulp.task('sprite', function () {
+  var spriteData = gulp.src(src+'/sprite/*.png').pipe(spritesmith({
+    imgName: 'sprite.png',
+    cssName: '_sprite.scss'
+  }));
+  // Pipe image stream through image optimizer and onto disk 
+  var imgStream = spriteData.img
+    // DEV: We must buffer our stream into a Buffer for `imagemin` 
+    .pipe(buffer())
+    .pipe(imagemin())
+    .pipe(gulp.dest(dst+"/img/"));
+ 
+  var cssStream = spriteData.css
+    .pipe(gulp.dest(src+'/scss/components/'));
+ 
+  // Return a merged stream to handle both `end` events 
+  return merge(imgStream, cssStream);
+});
+
+gulp.task('reloadHTML', function() {
+  return gulp.src(dst+'/**/*.html')
+	.pipe(connect.reload());
+});
+
+//Watch task
+gulp.task('default',function() {
+	connect.server({ root: dst,
+    livereload: true,
+	port: 8888});
+    gulp.watch(src+'/scss/**/*.scss', ['concat-styles']);
+	gulp.watch(src+'/js/**/*.js', ['js']);
+	gulp.watch('img/**/*', {cwd: src}, ['images']);
+	gulp.watch('fonts/**/*', {cwd: src}, ['fonts']);
+	gulp.watch(src+'/**/*.html', ['html']);
+});
